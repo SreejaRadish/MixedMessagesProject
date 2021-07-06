6f7dd753382b602dae75f6c54a3fda059f094bab
Sreeja R <SreejaRadish@gmail.com>
2021-07-06 14:31:28 +0100

    second commit

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
+const emotion = {
+    emotionType : ['Happiness','Sadness','Anger'],
+    happyMessages : ['Spread Happiness','Help Others','Spread Joy'],
+    sadMessages : ['Stay strong','This will pass too','You are awesome','You are Unique'],
+    angerMessages : ['Calm Down' ,'Dont hurt Yourself','Calmness is a super power'],
+    randomNum(num){
+        return Math.floor(Math.random()* num );
+    },
+   
+    createMessage(){
+        let emotionIndex=this.randomNum(this.emotionType.length);
+        let messageIndex=undefined;
+        switch(emotionIndex) {
+          case 0 :
+            messageIndex = this.randomNum(this.happyMessages.length);
+            return `You are happy now so ${this.happyMessages[messageIndex]}`;
+         case 1 :
+            messageIndex = this.randomNum(this.sadMessages.length);
+            return `Don't worry ${this.sadMessages[messageIndex]}`;   
+         case 2 :
+            messageIndex = this.randomNum(this.angerMessages.length);
+            return `Don't be angry ${this.angerMessages[messageIndex]}`;     
+         default :
+            return 'No Emotions';   
+      }
 
- const basicEmotions = ['Happiness','Sadness'];
-const messages = {
-    happyMessages: ['Spread Happiness','Help Others','Spread Joy'],
-    sadMessages : ['Stay strong','This will pass too','You are awesome','You are Unique']
-};
-let finalMessage=[];
-let randomNum = (num) => {
-return Math.floor(Math.random()* num );
-};
- let selectEmotion=() => {
-for (let emotion in basicEmotions ){
-    switch(emotion) {
-        case 'Happiness' :
-            //InspiringMessage.push()
-            console.log('Happy');
-        
-        case 'Sadness' :
-            console.log('Happy');
-       
-        default :
-        console.log('No Emotions');  
-        break;   
 
     }
-}
 };
-console.log(selectEmotion());
+console.log(emotion.createMessage());
